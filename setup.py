6d96d0d996414e33506221730fe946c74ad90e44
Mattia Lizzano <mattia.lizzano@iit.it>
2024-08-20 11:24:36 +0200

    Modified for pip installation

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="EM_data_analysis",
-    version="1.0",
-    description="This package contains several modules for Electron Microscopy data analysis",
-    author="Mattia Lizzano",
-    author_email="mattializzano@gmail.com",
-    url="https://github.com/Nomeutente1/EM_data_analysis",
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    name='EM_data_analysis',  
+    version='1.0',  
+    packages=find_packages(),  
+    install_requires=[
+        'opencv-python',       
+        'numpy',               
+        'porespy',             
+        'matplotlib',          
+        'openpnm',             
+        'scipy',              
+        'pandas',              
+        'dataframe-image',     
+        'moviepy',             
+        'IPython',             
+        'scikit-image',        
     ],
-    python_requires='>=3.6',
+    description='A Python package for EM data analysis',
+    author='Mattia Lizzano',
+    author_email='mattializzano@gmail.com',
+    url='https://github.com/Nomeutente1/EM_data_analysis',  
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  
 )
 
