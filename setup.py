048bbdfc40fa3ac007ae38782f76a307c47db567
Mattia Lizzano <mattia.lizzano@iit.it>
2024-08-28 10:18:25 +0200

    Version 1.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='EM_data_analysis',  
-    version='1.0.3',  
+    version='1.0.4',  
     packages=find_packages(),  
     install_requires=[
         'opencv-python',       
